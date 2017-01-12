d3b89e83b4f62d9f89d9995393c55ba385224613
Tom  Christian <tctc91@gmail.com>
2017-01-12 00:07:02 +0000

    basic components setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.stationList = null;
+    this.handleStationSearch = this.handleStationSearch.bind(this);
+    this.state = { stationName: '', stationList: [] };
+  }
+
+  handleStationSearch(evt) {
+    let name = evt.target.value;
+
+    this.setState({ stationName: name });
+
+    this.searchStation(evt.target.value).then((data) => {
+      this.setState({ stationList: data.matches });
+    });
+
+  }
+
+  searchStation(name) {
+    return fetch('/public/data/mock-data.json', { method: 'get' })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log('error: ', err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Welcome to My Stop</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <StationForm stationName={this.state.stationName} onChange={this.handleStationSearch} />
+        <StationList stationList={this.state.stationList} />
+      </div>
+    );
+  }
+}
+
+class StationForm extends Component {
+  render() {
+    return (
+      <label htmlFor="station">
+        Enter a station name:
+        <input id="station" value={this.props.stationName} onChange={this.props.onChange} />
+      </label>
+    );
+  }
+}
+
+class StationList extends Component {
+  render() {
+    const stationList = this.props.stationList.map((station) =>
+      <p key={station.id}>{station.name}</p>
+    );
+
+    return (
+      <div>stations:
+      {stationList}
       </div>
     );
   }
